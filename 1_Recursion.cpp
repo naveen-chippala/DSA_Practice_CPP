54fcdcd640a6f8634c6671387bcd646966c7ba18
naveen <chippalanaveen1@gmail.com>
2024-01-23 18:20:04 +0530

    finding n power p using recursion

---
+++ 1_Recursion.cpp
@@ -0,0 +1,21 @@
+//finding n power p value using recursion
+#include<iostream>
+using namespace std;
+int pow(double n, double p){
+if(n==0 && p==0){
+    return 0;
+}
+else if(n>=1 && p==0){
+        return 1;
+}
+else if (n>1 && p>=1){
+return n*pow(n,p-1);//
+}
+}
+int main(){
+double ans,n,p;
+cout<<"enter the n and p values"<<endl;//to calculate n power p value
+cin>>n>>p;
+ans= pow(n,p);
+cout<<n<<"power"<<p<<"is"<<ans;
+}
